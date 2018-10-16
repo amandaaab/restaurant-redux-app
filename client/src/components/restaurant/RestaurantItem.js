083a8ce4ae99b789274f2bfb9b8111df427b2440
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-10-16 16:21:38 +0200

    styled components restaurantitem

--- client/src/components/restaurant/RestaurantItem.js
+++ client/src/components/restaurant/RestaurantItem.js
@@ -3,62 +3,111 @@ import SaveRestaurant from '../../containers/SaveRestaurant';
 import ReviewParent from '../../containers/ReviewParent';
 import CalculateRate from '../CalculateRate';
 import { FaArrowLeft } from 'react-icons/fa';
+import styled from 'styled-components';
+
 
 
 const RestaurantItem = (props) => {
       
 
-        return (
+    return (
             // Filter restaurant where one restaurant name matches the clicked restaurant name, restaurantP is sent from router
             props.restaurant.filter(restaurant => restaurant.name === props.restaurantP).map((restaurant) => 
 
-                <div className="restaurantItem">
-                <div>
-                    <div className="closeBtn" onClick={props.handleClose}><FaArrowLeft className="faArrowLeft"/></div>
-                    </div>
-                    <div>
-                    <img alt="restaurant" className="modalImage" src={require(`../../images/${restaurant.img}`)} />
-    
-                    <div className="infoWrapper">  
-                  
-                        <div className="boxOne">
-                        <h3 className="nameR">{restaurant.name}</h3>
-                            <div className="info">{restaurant.category}</div>
-                            <div className="info">{restaurant.web}</div>
-                            <div className="info">{restaurant.address}</div>
-                        </div>
-
-                        <div className="boxTwo">
-                            <div className="rateDiv">
-                              <CalculateRate 
-                                        reviews={props.reviews}
-                                        id={restaurant.id} 
-                                        />
-                            </div>
-                              <div className="saveDiv">
-                              <SaveRestaurant restaurantToSave={restaurant} />
-                             
-                              </div>
-                          
-                        </div>
-    
-                    </div>
-    
-                      <ReviewParent id={restaurant.id}/>              
+                <Container>
                             
-                  </div>
-                  </div>
+                    <CloseBtn onClick={props.handleClose}><FaArrowLeft className="faArrowLeft"/></CloseBtn>
+                        
+                    <div>
+                            <img alt="restaurant" className="modalImage" src={require(`../../images/${restaurant.img}`)} />
+            
+                            <InfoWrapper>  
+                        
+                                        <LeftBox>
+                                            <h3 className=".name">{restaurant.name}</h3>
+                                            <div>{restaurant.category}</div>
+                                            <div >{restaurant.web}</div>
+                                            <div >{restaurant.address}</div>
+                                        </LeftBox>
+
+                                        <RightBox>
+                                                <div className="rateDiv">
+                                                        <CalculateRate 
+                                                            reviews={props.reviews}
+                                                            id={restaurant.id} 
+                                                            />
+                                                </div>
+
+                                                <div className="saveDiv">
+                                                        <SaveRestaurant restaurantToSave={restaurant} />
+                                                </div>
+                                        
+                                        </RightBox>
+            
+                            </InfoWrapper>
+            
+                            <ReviewParent id={restaurant.id}/>              
+                            
+                    </div>
+                </Container>
                   
                 
-              )
+            )
 
-              
-
-      )  
+    )  
 
 }
 
-
-
-
 export default RestaurantItem;
+
+
+
+const Container = styled.div`
+    /*display: flex;
+    justify-content: center;*/
+`
+
+
+const CloseBtn = styled.div`
+    width: 80px;
+    position: relative;
+    top:0;
+`
+
+
+
+const LeftBox = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    .name {
+        margin-left: 35px;
+    }
+
+`
+
+const RightBox = styled.div`
+    width: 30%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 0px 34px;
+
+    .rateDiv {
+        margin: 4px;
+        width: 150px;
+    }
+    .saveDiv {
+        margin: 4px;
+
+    }
+`
+
+
+const InfoWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    height: 200px auto;
+    width: 100%;
+`
